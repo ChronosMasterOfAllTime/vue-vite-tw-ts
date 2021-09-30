67338f900bb09ddc055531963a5bb1b5a9d6ddbe
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2021-09-30 09:14:08 -0500

    use sassy sass

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,9 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+<style lang="sass">
+#app
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -41,20 +41,17 @@ const increment = () => store.dispatch('increment')
   </p>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
+<style lang="sass" scoped>
+a
+  color: #42b983
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
+label
+  margin: 0 0.5em
+  font-weight: bold
 
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
+code
+  background-color: #eee
+  padding: 2px 4px
+  border-radius: 4px
+  color: #304455
 </style>
