01e61edcbd32bad14ecef0f4fa7a890fcf6c11c2
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2021-09-30 08:34:29 -0500

    add VariantJS (aka vue-tailwind)

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import App from '@/App.vue'
 import '@/styles/index.sass'
 import store, { key } from '@/store'
 import router from '@/router'
+import { variantJS, VariantJSConfiguration } from '@variantjs/vue'
 
 const app = createApp(App)
 
 app.use(store, key)
 app.use(router)
+
+const config: VariantJSConfiguration = {}
+
+app.use(variantJS, config)
 
 app.mount('#app')
