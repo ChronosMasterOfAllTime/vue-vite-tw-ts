e62ce6435768a52329ffbd5e05a413250e7dbb35
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-02-18 16:32:20 -0600

    switch from Vuex to pinia

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import '@/styles/index.sass'
-import store, { key } from '@/store'
 import router from '@/router'
 import { variantJS, VariantJSConfiguration } from '@variantjs/vue'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
-
-app.use(store, key)
 app.use(router)
+app.use(createPinia())
 
 const config: VariantJSConfiguration = {}
 
---
+++ src/store/main.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+// useStore could be anything like useUser, useCart
+// the first argument is a unique id of the store across your application
+export const useMainStore = defineStore('main', {
+  state: () => {
+      return {
+          counter: 0
+      }
+  },
+  getters: {
+      count: ({counter}) => counter
+  },
+  actions: {
+      incrementCounter(count: number) {
+          this.counter += count
+      }
+  }
+})
