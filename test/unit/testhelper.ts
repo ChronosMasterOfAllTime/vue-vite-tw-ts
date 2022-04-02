b3b57629fdd6c309099e90a5c92c2ffec219fb50
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-04-02 16:13:21 -0500

    some test updates

--- test/unit/testhelper.ts
+++ test/unit/testhelper.ts
@@ -1,18 +1,24 @@
 import { mount, MountingOptions } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
-import { DefineComponent } from 'vue'
+import { DefineComponent, Plugin } from 'vue'
 import { globalComponents } from '@/components'
+import { setActivePinia } from 'pinia'
 
 export function mountComponent<T extends InstanceType<DefineComponent>>(
   component: T,
   options: MountingOptions<T> = { shallow: true }
 ) {
+  const pinia = createTestingPinia({ createSpy: vi.fn })
+  setActivePinia(pinia)
+
+  const plugins: Array<Plugin> = [pinia, globalComponents]
+
   if (options.global?.plugins) {
-    options.global.plugins.push(createTestingPinia({ createSpy: vi.fn }))
+    options.global.plugins.push(...plugins)
   } else {
     options.global = {
       ...(options.global || {}),
-      plugins: [createTestingPinia({ createSpy: vi.fn }), globalComponents]
+      plugins
     }
   }
 
--- test/unit/views/HelloWorld.spec.ts
+++ test/unit/views/HelloWorld.spec.ts
@@ -1,13 +1,14 @@
 import HelloWorld from '@/views/HelloWorld.vue'
 import { mountComponent } from '@test/unit/testhelper'
 import { VueWrapper } from '@vue/test-utils'
+import { NButton } from 'naive-ui'
 
 describe('HelloWorld.vue', () => {
   let wrapper: VueWrapper<InstanceType<typeof HelloWorld>>
   beforeEach(() => {
     wrapper = mountComponent<InstanceType<typeof HelloWorld>>(HelloWorld, {
       props: { msg: 'foo bar' },
-      shallow: true
+      shallow: false
     })
   })
 
@@ -17,5 +18,20 @@ describe('HelloWorld.vue', () => {
 
   test('should mount', () => {
     expect(wrapper.findComponent(HelloWorld).exists()).toBe(true)
+  })
+
+  test('should click a button', async () => {
+    let nButton = wrapper.findComponent(NButton)
+
+    expect(nButton.exists()).toBe(true)
+
+    for (let i = 0; i < 61; i++) {
+      nButton.trigger('click')
+      await wrapper.vm.$nextTick()
+    }
+
+    // nButton = wrapper.findComponent(NButton)
+
+    // expect(wrapper.vm.main.incrementCounter).toHaveBeenCalledTimes(30)
   })
 })
