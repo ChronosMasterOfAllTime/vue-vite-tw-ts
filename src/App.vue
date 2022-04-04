ec8bb6881fa35ed00cbad0d3854e3ad75c8f4986
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-04-03 20:33:34 -0500

    add media query for dark mode

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 import { darkTheme, lightTheme } from 'naive-ui'
 import { computed, ref } from 'vue'
 
-const mode = ref<'dark' | 'light'>('dark')
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+const mode = ref<'dark' | 'light'>(prefersDark ? 'dark' : 'light')
 const theme = computed(() => (mode.value === 'dark' ? darkTheme : lightTheme))
 </script>
 
@@ -10,12 +12,13 @@ const theme = computed(() => (mode.value === 'dark' ? darkTheme : lightTheme))
   <n-config-provider :theme="theme">
     <n-layout position="absolute">
       <n-layout-header style="height: 64px; padding: 24px" bordered>
-        Yiheyuan Road
+        Layout Header
       </n-layout-header>
       <n-layout
         class="bg-white dark:bg-gray-800 dark:text-white text-gray-800 text-center h-screen w-screen"
         has-sider
         position="absolute"
+        style="top: 64px; bottom: 64px"
       >
         <n-layout-sider
           collapse-mode="transform"
@@ -23,7 +26,7 @@ const theme = computed(() => (mode.value === 'dark' ? darkTheme : lightTheme))
           show-trigger="arrow-circle"
           bordered
         >
-          <n-h2>Handian Bridge</n-h2>
+          <n-h2>Layout Sider</n-h2>
         </n-layout-sider>
         <n-layout-content>
           <router-view />
