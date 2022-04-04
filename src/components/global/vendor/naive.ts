fa2bb21409da4119f55c40b03ca398bf23acfd3b
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-04-03 20:11:38 -0500

    add more layout plugins to naive.ts

--- src/components/global/vendor/naive.ts
+++ src/components/global/vendor/naive.ts
@@ -1,12 +1,38 @@
 import {
   create,
   NButton,
+  NConfigProvider,
+  NH1,
+  NH2,
+  NH3,
+  NH4,
+  NH5,
+  NH6,
+  NHr,
   NLayout,
   NLayoutContent,
+  NLayoutFooter,
+  NLayoutHeader,
   NLayoutSider,
   NMenu
 } from 'naive-ui'
 
 export default create({
-  components: [NButton, NLayout, NLayoutContent, NLayoutSider, NMenu]
+  components: [
+    NButton,
+    NConfigProvider,
+    NH1,
+    NH2,
+    NH3,
+    NH4,
+    NH5,
+    NH6,
+    NHr,
+    NLayout,
+    NLayoutHeader,
+    NLayoutContent,
+    NLayoutFooter,
+    NLayoutSider,
+    NMenu
+  ]
 })
