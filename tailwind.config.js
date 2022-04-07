3c51a071570e89d67d6bd8a82a238be6262288d5
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-04-06 21:31:52 -0500

    remove defaults from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'media', // false, 'media' or 'class'
-  mode: 'jit',
   theme: {
-    extend: {}
-  },
-  variants: {
     extend: {}
   },
   plugins: [require('@tailwindcss/forms')]
