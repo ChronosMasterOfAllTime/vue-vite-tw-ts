9f4d0e77e94c4c7b1882ef36442f60744888a958
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-09-21 21:21:32 -0500

    modify vite/vitest configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,32 @@
+import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import config from './vite.config'
 
 const plugins = [vue(), tsconfigPaths({ root: 'test/unit' })]
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins,
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    coverage: {
-      all: true,
-      exclude: ['*.config.{ts,js}', '**/*.d.ts', 'src/main.ts', 'dist', 'test']
+export default mergeConfig(
+  config,
+  defineConfig({
+    plugins,
+    test: {
+      deps: {
+        inline: ['date-fns']
+      },
+      environment: 'happy-dom',
+      globals: true,
+      coverage: {
+        all: true,
+        exclude: [
+          '*.config.{ts,js}',
+          '**/*.d.ts',
+          'src/main.ts',
+          'dist',
+          'test'
+        ]
+      }
     }
-  }
-})
+  })
+)
