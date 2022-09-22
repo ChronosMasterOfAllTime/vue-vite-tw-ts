122fd98cc89abd400359090c2d064a205de6acd9
ChronosMasterOfAllTime <efstathiosc@gmail.com>
2022-09-21 21:52:58 -0500

    max out test coverage and fix inner tsconfig

--- test/unit/testhelper.ts
+++ test/unit/testhelper.ts
@@ -1,13 +1,15 @@
+import { globalComponents } from '@/components'
+import router from '@/router'
 import { mount, MountingOptions } from '@vue/test-utils'
 import { createTestingPinia, TestingOptions } from '@pinia/testing'
 import { ComponentPublicInstance, Plugin } from 'vue'
-import { globalComponents } from '@/components'
 import { createPinia, setActivePinia } from 'pinia'
 
 export function mountComponent<T extends ComponentPublicInstance>(
   component: T,
   options: MountingOptions<T> = { shallow: false },
-  mockStore?: TestingOptions
+  mockStore?: TestingOptions,
+  useRouter = false
 ) {
   const pinia = mockStore
     ? createTestingPinia({ createSpy: vi.fn, ...mockStore })
@@ -15,6 +17,10 @@ export function mountComponent<T extends ComponentPublicInstance>(
   setActivePinia(pinia)
 
   const plugins: Array<Plugin> = [pinia, globalComponents]
+
+  if (useRouter) {
+    plugins.push(router)
+  }
 
   if (options.global?.plugins) {
     options.global.plugins.push(...plugins)
