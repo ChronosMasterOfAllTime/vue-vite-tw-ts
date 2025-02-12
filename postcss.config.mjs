525127e1b2999bcb2cb1172cdffdd1f9217ae809
Jay Lapham <jay.lapham@thomsonreuters.com>
2025-02-12 11:57:12 -0600

    next big upgrade for npm packages, sass warnings begone, need to fix stylesheets not getting applied somewhere

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    // "tailwindcss": {},
+    "@tailwindcss/postcss": {},
+    //"@tailwindcss/nesting": {},
+    "autoprefixer": {},
+    "postcss-import": {}
+  }
+}
